233c35120af05e8ad28973d55904c1164daa5453
github username <github email address>
2018-10-10 13:10:01 -0400

    Done.

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ class Route {
   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     let travelled = 0
-  travelled = travelled + (eastWest.indexOf(this.endingLocation['horizontal']) - eastWest.indexOf(this.beginningLocation['horizontal']))
-  travelled = travelled + (this.endingLocation['vertical'] - this.beginningLocation['vertical'])
-  return travelled
+    travelled = travelled + (eastWest.indexOf(this.endingLocation['horizontal']) - eastWest.indexOf(this.beginningLocation['horizontal']))
+    travelled = travelled + (this.endingLocation['vertical'] - this.beginningLocation['vertical'])
+    return travelled
     }
-    estimatedTime(rushHour) {
-      if (rushHour === true) {
-        return this.blocksTravelled() / 2
-      } else {
-        return this.blocksTravelled() / 3
-      }
+  estimatedTime(rushHour) {
+    if (rushHour === true) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
     }
+  }
 }
