21143426312020f1df4bd128c58b917cf9836c8d
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-11 15:22:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(start, end) {
+    this.start = start
+    this.end = end
+  }
+	
+  blocksTravelled() {
+		let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+		//debugger
+		const x_end = eastWest.indexOf(this.end.horizontal)
+		const x_start = eastWest.indexOf(this.start.horizontal)
+    const x = Math.abs(x_end - x_start)
+    const y = Math.abs(parseInt(this.end.vertical) - parseInt(this.start.vertical))
+    return x + y
+  }
+
+  estimatedTime(peak) {
+    let modifier = 0
+    peak ? modifier = 2 : modifier = 3
+    return this.blocksTravelled() / modifier
+  }
+}
